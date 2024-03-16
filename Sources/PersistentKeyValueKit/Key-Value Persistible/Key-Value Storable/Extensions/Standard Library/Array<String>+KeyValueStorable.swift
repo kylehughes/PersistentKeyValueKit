ea324145e9de09f1cecea3ba09c0b6a76f35b099
Kyle Hughes <me@kylehugh.es>
2024-03-16 02:19:38 -0700

    more docs

--- Sources/PersistentKeyValueKit/Key-Value Persistible/Key-Value Storable/Extensions/Standard Library/Array<String>+KeyValueStorable.swift
+++ Sources/PersistentKeyValueKit/Key-Value Persistible/Key-Value Storable/Extensions/Standard Library/Array<String>+KeyValueStorable.swift
@@ -17,6 +17,11 @@ extension Array<String>: KeyValueStorable {
     
     // MARK: Interfacing With User Defaults
 
+    /// Get the value, as `Storage`, at the given key from the given `UserDefaults`.
+    ///
+    /// - Parameter userDefaultsKey: The key to get the value from.
+    /// - Parameter userDefaults: The `UserDefaults` to get the value from, as `Storage`, at `userDefaultsKey`.
+    /// - Returns: The value, as `Storage`, at `userDefaultsKey` in `userDefaults`, if it exists.
     @inlinable
     public static func get(_ userDefaultsKey: String, from userDefaults: UserDefaults) -> Storage? {
         userDefaults.stringArray(forKey: userDefaultsKey)
@@ -33,7 +38,13 @@ extension Array<String>: KeyValueStorable {
     
     #if !os(watchOS)
 
-    
+    // MARK: Interfacing With Ubiquitous Key-Value Store
+
+    /// Get the value, as `Storage`, at the given key from the given `NSUbiquitousKeyValueStore`.
+    ///
+    /// - Parameter ubiquitousStoreKey: The key to get the value from.
+    /// - Parameter ubiquitousStore: The `NSUbiquitousKeyValueStore` to get the value from, as `Storage`, at
+    ///   `ubiquitousStoreKey`.
     @inlinable
     public static func get(
         _ ubiquitousStoreKey: String,
@@ -42,6 +53,11 @@ extension Array<String>: KeyValueStorable {
         ubiquitousStore.array(forKey: ubiquitousStoreKey) as? Storage
     }
     
+    /// Set the value, as `Storage`, at the given key in the given `NSUbiquitousKeyValueStore`.
+    ///
+    /// - Parameter ubiquitousStoreKey: The key to set the value at.
+    /// - Parameter ubiquitousStore: The `NSUbiquitousKeyValueStore` to set the value in, as `Storage`, at
+    ///   `ubiquitousStoreKey`.
     @inlinable
     public func set(
         as ubiquitousStoreKey: String,
