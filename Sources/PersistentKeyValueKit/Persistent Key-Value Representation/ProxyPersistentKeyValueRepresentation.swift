68e3f3e0350b576bc65c055ed8baf1df80c7b0fd
Kyle Hughes <me@kylehugh.es>
2024-04-20 12:28:18 -0700

    Remove unnecessary property?

--- Sources/PersistentKeyValueKit/Persistent Key-Value Representation/ProxyPersistentKeyValueRepresentation.swift
+++ Sources/PersistentKeyValueKit/Persistent Key-Value Representation/ProxyPersistentKeyValueRepresentation.swift
@@ -11,8 +11,6 @@ public protocol ProxyablePersistentKeyValueRepresentation<Value, Proxy>: Persist
     associatedtype Proxy: KeyValuePersistible
     associatedtype Value
     
-    var proxyRepresentation: Proxy.PersistentKeyValueRepresentation { get }
-    
     func deserializing(_ proxy: Proxy) -> Value?
     func serializing(_ value: Value) -> Proxy
 }
@@ -24,7 +22,7 @@ extension ProxyablePersistentKeyValueRepresentation {
     
     @inlinable
     public func get(_ userDefaultsKey: String, from userDefaults: UserDefaults) -> Value? {
-        guard let proxyValue = proxyRepresentation.get(userDefaultsKey, from: userDefaults) else {
+        guard let proxyValue = Proxy.persistentKeyValueRepresentation.get(userDefaultsKey, from: userDefaults) else {
             return nil
         }
         
@@ -33,7 +31,7 @@ extension ProxyablePersistentKeyValueRepresentation {
     
     @inlinable
     public func set(_ userDefaultsKey: String, to value: Value, in userDefaults: UserDefaults) {
-        proxyRepresentation.set(userDefaultsKey, to: serializing(value), in: userDefaults)
+        Proxy.persistentKeyValueRepresentation.set(userDefaultsKey, to: serializing(value), in: userDefaults)
     }
     
     // MARK: Interface with Ubiquitous Key-Value Store
@@ -41,7 +39,7 @@ extension ProxyablePersistentKeyValueRepresentation {
     @inlinable
     public func get(_ ubiquitousStoreKey: String, from ubiquitousStore: NSUbiquitousKeyValueStore) -> Value? {
         guard
-            let proxyValue = proxyRepresentation.get(ubiquitousStoreKey, from: ubiquitousStore)
+            let proxyValue = Proxy.persistentKeyValueRepresentation.get(ubiquitousStoreKey, from: ubiquitousStore)
         else {
             return nil
         }
@@ -51,13 +49,12 @@ extension ProxyablePersistentKeyValueRepresentation {
     
     @inlinable
     public func set(_ ubiquitousStoreKey: String, to value: Value, in ubiquitousStore: NSUbiquitousKeyValueStore) {
-        proxyRepresentation.set(ubiquitousStoreKey, to: serializing(value), in: ubiquitousStore)
+        Proxy.persistentKeyValueRepresentation.set(ubiquitousStoreKey, to: serializing(value), in: ubiquitousStore)
     }
 }
 
 public struct ProxyPersistentKeyValueRepresentation<Value, Proxy> where Proxy: KeyValuePersistible {
     public let deserializing: (Proxy) -> Value?
-    public let proxyRepresentation: Proxy.PersistentKeyValueRepresentation
     public let serializing: (Value) -> Proxy
     
     // MARK: Public Initialization
@@ -66,8 +63,6 @@ public struct ProxyPersistentKeyValueRepresentation<Value, Proxy> where Proxy: K
     public init(serializing: @escaping (Value) -> Proxy, deserializing: @escaping (Proxy) -> Value?) {
         self.serializing = serializing
         self.deserializing = deserializing
-        
-        proxyRepresentation = Proxy.persistentKeyValueRepresentation
     }
 }
 
